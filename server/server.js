34cd2e55172c293e5010644d36b2ebc7a9eebaed
JolleJ <Joeljansson96@gmail.com>
2019-10-24 18:36:25 +0300

    basic server start

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+//reverse HOST for docker image
+//const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('');
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
